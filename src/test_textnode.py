2c8103aeb3ad02063c44d32363abbbf3ba8730dd
FJDubs <taggartf1@gmail.com>
2024-10-07 17:22:41 -0400

    TextNodes can now be converted into LeafNodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode
+from leafnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,7 +23,35 @@ class TestTextNode(unittest.TestCase):
     def test_print(self):
         node = TextNode('TextNode1', 'bold', 'https://github.com/FJDubs')
         self.assertEqual('TextNode(TextNode1, bold, https://github.com/FJDubs)', repr(node))
+    
+    def test_text_to_leaf(self):
+        node = TextNode('this is text', 'text')
+        self.assertEqual(node.text_node_to_html_node().__repr__(), LeafNode(None, 'this is text').__repr__())
 
+    def test_bold_to_leaf(self):
+        node = TextNode('this is bold', 'bold')
+        self.assertEqual(node.text_node_to_html_node().__repr__(), LeafNode('b', 'this is bold').__repr__())
+
+    def test_italic_to_leaf(self):
+        node = TextNode('this is italic', 'italic')
+        self.assertEqual(node.text_node_to_html_node().__repr__(), LeafNode('i', 'this is italic').__repr__())
+
+    def test_code_to_leaf(self):
+        node = TextNode('this is code', 'code')
+        self.assertEqual(node.text_node_to_html_node().__repr__(), LeafNode('code', 'this is code').__repr__())
+
+    def test_link_to_leaf(self):
+        node = TextNode('this is a link', 'link', 'boot.dev')
+        self.assertEqual(node.text_node_to_html_node().__repr__(), LeafNode('a', 'this is a link', {'href': 'boot.dev'}).__repr__())
+
+    def test_img_to_leaf(self):
+        node = TextNode('this is an image', 'image', 'boot.dev')
+        self.assertEqual(node.text_node_to_html_node().__repr__(), LeafNode('img', '', {'src': 'boot.dev', 'alt': 'this is an image'}).__repr__())
+
+    def test_unsuported(self):
+        node = TextNode('this should error', 'error')
+        with self.assertRaisesRegex(ValueError, 'Text type is not supported'):
+            node.text_node_to_html_node()
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
+from leafnode import LeafNode
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text, text_type=None, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -13,3 +14,20 @@ class TextNode:
     
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+    
+    def text_node_to_html_node(self):
+        match self.text_type:
+            case 'text':
+                return LeafNode(None, self.text)
+            case 'bold':
+                return LeafNode('b', self.text)
+            case 'italic':
+                return LeafNode('i', self.text)
+            case 'code':
+                return LeafNode('code', self.text)
+            case 'link':
+                return LeafNode('a', self.text, {'href': self.url})
+            case 'image':
+                return LeafNode('img', '', {'src': self.url, 'alt': self.text})
+            case _:
+                raise ValueError('Text type is not supported')
